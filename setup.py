234fcd4c218ebd80ab1982842580e36fd304498f
JeffersonQin <gyrojeff@foxmail.com>
2023-12-05 20:19:59 -0500

    feat: update package version to make everything working together

--- setup.py
+++ setup.py
@@ -15,6 +15,6 @@ setup(
                       "gunicorn==19.9.0", "hydra-core==1.2.0", "imageio==2.9.0", "matplotlib==3.3.4", "numpy==1.19.5",
                       "opencv-python==4.5.1.48", "pandas==1.4.3", "Paste==3.5.0", "PasteDeploy==2.1.1", "Pillow==8.2.0", "psutil==5.9.3",
                       "progress==1.6", "protobuf==3.13.0", "pymeshlab==0.2.1", "pyrender==0.1.45", "pytimeparse==1.1.8", 
-                      "pyglet==1.5.20", "plyfile==0.7.4", "scipy==1.5.4", "requests==2.22.0", "tqdm==4.64.0", "trimesh==3.9.16"]
+                      "pyglet==1.5.20", "plyfile==0.7.4", "scipy==1.5.4", "requests==2.22.0", "tqdm==4.64.0", "trimesh==3.9.16", "Jinja2==3.0.3", "itsdangerous==2.0.1", "werkzeug==2.0.3"]
 )
 
